2cdf9340c2eace437b46aeae91bce85042837de7
smgyx <144696649+smgyx@users.noreply.github.com>
2024-06-01 16:44:15 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>网站名称</title> 
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="Keywords" content="软件名称" />
+<meta name="Description" content="软件名称" />
+<link rel="shortcut icon" href="/img/favicon.ico" />
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="hedadr">
+    <img src="logo.jpg">
+    <ul>软件名称</ul>
+    <p>版本:1.0  大小:MB</p>
+    <dd><a href="跳转下载链接"><button>点击下载</button></a></dd>
+    <dd><a href="跳转加群链接"><button>点击加群</button>
+  </div>
+  <div class="title">软件截图</div>
+  <div class="show">
+    <div class="pic">
+      <img src="app1.png">
+      <img src="app2.png">
+      <img src="app3.png">
+      <img src="app4.png">
+      <img src="app5.png">
+    </div>
+  </div>
+  <div class="title">软件介绍</div>
+  <div class="show">
+自定义内容
+  </div>
+  <div>
+  <div class="title" id="q">开发者信息</div>
+  <div class="show" id="qx">
+  交流群: <font color="blue">群号</font>
+</div>
+</body>
+</html>
